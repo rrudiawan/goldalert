909c9994e2a79f24575fac7450018688b656ccc8
rrudiawan <rrudiawan@gmail.com>
2026-09-15 03:57:47 +0700

    Add global pricing, sentiment, widget and PWA upgrades

--- dist/sw.js
+++ dist/sw.js
@@ -1,7 +1,7 @@
-const CACHE="goldalert-shell-v3";
+const CACHE="goldalert-shell-v4";
 const SHELL=["./","index.html","methodology.html","about.html","contact.html","privacy.html","terms.html",
   "articles.html","articles/read-gold-buy-signal.html","articles/gold-stocks-etfs-crypto.html",
-  "static-page.css","manifest.webmanifest","icons/logo.svg","icons/icon-192.png"];
+  "static-page.css","enhancements.js","manifest.webmanifest","icons/logo.svg","icons/icon-192.png","embed/index.html","widget/index.html"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{
