f0e7564ec669d085b547e089e5dfe00060523181
rrudiawan <rrudiawan@gmail.com>
2026-09-18 06:11:06 +0700

    Add Decap CMS admin panel at /admin/

--- dist/sw.js
+++ dist/sw.js
@@ -7,6 +7,8 @@ const SHELL=["./","index.html","methodology.html","about.html","contact.html","p
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{
+  if(new URL(event.request.url).pathname.startsWith("/admin/")) return;
+
   if(event.request.method!=="GET"||new URL(event.request.url).origin!==location.origin) return;
   event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request)));
 });
